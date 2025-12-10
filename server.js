338213e54d4e4914dca1f02b508cb594fd41359f
Nicola Preda <nicola.preda05@gmail.com>
2025-12-11 00:59:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,13 @@ const app = express();
 // Serve i file statici (HTML, CSS, JS, immagini)
 app.use('/typedesign', express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
+
 const PORT = process.env.PORT || 3011;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server attivo su http://0.0.0.0:${PORT}`));
